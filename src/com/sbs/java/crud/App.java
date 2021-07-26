2130286186c70a8e27ad77fae9139af555510f82
woooyoung <axdsw121@gmail.com>
2021-07-26 12:33:56 +0900

    구조개선 (MemberController 도입)

--- src/com/sbs/java/crud/App.java
+++ src/com/sbs/java/crud/App.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.java.crud.controller.ArticleController;
+import com.sbs.java.crud.controller.MemberController;
 import com.sbs.java.crud.dto.Article;
 import com.sbs.java.crud.dto.Member;
 import com.sbs.java.crud.util.Util;
@@ -24,6 +26,11 @@ public class App {
 		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
+		
+		MemberController memberController = new MemberController(sc, members);
+		ArticleController articleController = new ArticleController();
+		
+		
 
 		while (true) {
 			System.out.printf("명령어) ");
@@ -39,48 +46,9 @@ public class App {
 				break;
 			}
 			if (command.equals("member join")) {
-				int id = members.size() + 1;
-				String regDate = Util.getNowDateStr();
-
-				String loginId = null;
-
-				while (true) {
-					System.out.printf("로그인 아이디 : ");
-					loginId = sc.nextLine();
-
-					if (isJoinableLoginId(loginId) == false) {
-						System.out.printf("%s는(은) 이미 사용중인 아이디 입니다.\n", loginId);
-						continue;
-					}
-
-					break;
-				}
-
-				String loginPw = null;
-				String loginPwConfirm = null;
-
-				while (true) {
-
-					System.out.printf("로그인 비밀번호 : ");
-					loginPw = sc.nextLine();
-
-					System.out.printf("로그인 비밀번호 확인: ");
-					loginPwConfirm = sc.nextLine();
-
-					if (loginPw.equals(loginPwConfirm) == false) {
-						System.out.println("비밀번호를 다시 입력해주세요.");
-						continue;
-					}
-
-					break;
-				}
-				System.out.printf("이름 : ");
-				String name = sc.nextLine();
-
-				Member member = new Member(id, regDate, loginId, loginPw, name);
-				members.add(member);
-
-				System.out.printf("%d번 회원이 생성되었습니다.\n", id);
+				memberController.doJoin();
+				
+				
 
 			} else if (command.equals("article write")) {
 				int id = articles.size() + 1;
@@ -195,27 +163,7 @@ public class App {
 		System.out.println("== 프로그램 끝 ==");
 	}
 
-	private boolean isJoinableLoginId(String loginId) {
-		int index = getMemberIndexByLoginId(loginId);
 
-		if (index == -1) {
-			return true;
-		}
-
-		return false;
-	}
-
-	private int getMemberIndexByLoginId(String loginId) {
-		int i = 0;
-		for (Member member : members) {
-			if (member.loginId.equals(loginId)) {
-				return i;
-			}
-			i++;
-		}
-
-		return -1;
-	}
 
 	private int getArticleIndexbyId(int id) {
 		int i = 0;
@@ -232,14 +180,6 @@ public class App {
 	}
 
 	private Article getArticleById(int id) {
-
-//		for (int i = 0; i < articles.size(); i++) {
-//			Article article = articles.get(i);
-//
-//			if (article.id == id) {
-//				return article;
-//			}
-//		}
 
 		int index = getArticleIndexbyId(id);
 
---
+++ src/com/sbs/java/crud/controller/ArticleController.java
@@ -0,0 +1,5 @@
+package com.sbs.java.crud.controller;
+
+public class ArticleController {
+
+}
