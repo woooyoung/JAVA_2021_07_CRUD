df9a72fcfb034d6aff5baba8b7c4f361ff24a6c8
woooyoung <axdsw121@gmail.com>
2021-07-26 11:23:51 +0900

    회원가입 (LoginId 중복검사)

--- src/com/sbs/java/crud/App.java
+++ src/com/sbs/java/crud/App.java
@@ -41,8 +41,20 @@ public class App {
 			if (command.equals("member join")) {
 				int id = members.size() + 1;
 				String regDate = Util.getNowDateStr();
-				System.out.printf("로그인 아이디 : ");
-				String loginId = sc.nextLine();
+
+				String loginId = null;
+
+				while (true) {
+					System.out.printf("로그인 아이디 : ");
+					loginId = sc.nextLine();
+
+					if (isJoinableLoginId(loginId) == false) {
+						System.out.printf("%s는(은) 이미 사용중인 아이디 입니다.\n", loginId);
+						continue;
+					}
+
+					break;
+				}
 
 				String loginPw = null;
 				String loginPwConfirm = null;
@@ -181,6 +193,28 @@ public class App {
 		sc.close();
 
 		System.out.println("== 프로그램 끝 ==");
+	}
+
+	private boolean isJoinableLoginId(String loginId) {
+		int index = getMemberIndexByLoginId(loginId);
+
+		if (index == -1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private int getMemberIndexByLoginId(String loginId) {
+		int i = 0;
+		for (Member member : members) {
+			if (member.loginId.equals(loginId)) {
+				return i;
+			}
+			i++;
+		}
+
+		return -1;
 	}
 
 	private int getArticleIndexbyId(int id) {
